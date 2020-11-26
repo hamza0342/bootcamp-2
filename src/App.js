3298f174021eb9e0d13593f203ff9c8167aa9780
hamza0342 <63608271+hamza0342@users.noreply.github.com>
2020-11-26 13:16:57 +0500

    Latest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import React, {useState} from 'react';
 import './App.css';
+import Student from './student';
+
 
 function App() {
+
+  let [count, setCount] = useState(0);
+  let [isDay, setDay] = useState(true);
   return (
-    <div >
-      Hello World from Hamza Toor
+    <div className= {`${isDay ? 'dayLight' : 'box'}`}>
+    <Student count={count}> </Student>
+
+    <h1>
+      Good {isDay ? 'Morning' : 'Night'}
+    </h1>
+    
+    <button onClick={ ()=> setCount(count=count+1) }>Click me</button>
+
+    <button onClick={()=>setDay(!isDay)}>Update background</button>
     </div>
   );
 }
---
+++ src/student.js
@@ -0,0 +1,18 @@
+
+import React, {useState} from 'react';
+
+const Student = (props) => {
+
+    
+    return(
+        <div>
+            <h1>
+                The marks of student are {props.count}
+
+                
+            </h1>
+        </div>
+    );
+}
+
+export default Student;
